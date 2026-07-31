4326b2749a7ed4993b508a56617042854805c9ad
debekehope <debekehope@gmail.com>
2026-07-31 04:52:31 +0100

    modified

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://98.83.39.85:8080',
+    baseURL:'http://54.234.137.78:8080',
     headers: {
         'Content-Type': 'application/json',
     },
